b839b1f62fd41af35b080f64b04d132dd19aa55f
binjing <jing.bin@dosomi.com>
2017-03-29 19:19:20 +0800

    支持Data类型

--- LevelDBForSwift/LevelDBForSwift/LevelDB.swift
+++ LevelDBForSwift/LevelDBForSwift/LevelDB.swift
@@ -10,13 +10,49 @@ import Foundation
 
 open class LevelDB {
     
-    public var db: UnsafeMutableRawPointer?
+    public private(set) var db: UnsafeMutableRawPointer?
     
-    public init(name: String) {
-        let filePath = NSHomeDirectory() + "/Documents/" + name
-        var cChar: [CChar] = [CChar].init(repeating: 0, count: 1024)
-        _ = filePath.getCString(&cChar, maxLength: 1024, encoding: .utf8)
+    public init(filePath: String) {
+        var cChar: [CChar] = [CChar].init(repeating: 0, count: 2048)
+        _ = filePath.getCString(&cChar, maxLength: 2048, encoding: .utf8)
         self.db = c_creatLeveldb(&cChar)
+    }
+    
+    convenience init(name: String) {
+        let filePath = NSHomeDirectory() + "/Documents/" + name
+        self.init(filePath: filePath)
+    }
+    
+    func set(_ value: Data, forKey key: String) {
+        let basePointer = UnsafeMutablePointer<Int8>.allocate(capacity: value.count)
+        basePointer.initialize(to: 0, count: value.count)
+        let pointer = UnsafeMutableBufferPointer<Int8>.init(start: basePointer, count: value.count)
+//        value.copyBytes(to: basePointer, count: value.count)
+        let count = value.copyBytes(to: pointer)
+        guard let db = self.db else {
+            return
+        }
+        guard var keyChar: [CChar] = key.cString(using: .utf8) else {
+            return
+        }
+        let keyCstring = _CString_(basePtr: &keyChar, lenght: keyChar.count)
+        let valueCstring = _CString_(basePtr: pointer.baseAddress, lenght: pointer.count)
+        c_leveldbSetValue(db, keyCstring, valueCstring)
+        basePointer.deallocate(capacity: pointer.count)
+    }
+    
+    func getData(forKey key: String) -> Data? {
+        guard let db = self.db else {
+            return nil
+        }
+        guard var keyChar: [CChar] = key.cString(using: .utf8) else {
+            return nil
+        }
+        var keyCstring = _CString_(basePtr: &keyChar, lenght: keyChar.count)
+        var valueString = c_leveldbGetValue(db, &keyCstring)
+        let data = Data.init(bytes: valueString.basePtr, count: valueString.lenght)
+        c_FreeCString(&valueString)
+        return data
     }
     
     subscript(key: String) -> String? {
